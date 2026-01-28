fe5b19f2e140025841a2ee0bc32f78a482db47de
Erwin <cerwiholding@gmail.com>
2026-01-28 14:37:25 +0100

    changed log

--- src/testing_agent/testing_agent.py
+++ src/testing_agent/testing_agent.py
@@ -23,7 +23,7 @@ class TestingAgent(CompetitionAgent):
     def __init__(self, faction: Faction, config):
         super().__init__(faction, config)
         
-        self.log("Constructed")
+        self.log("Constructed test 4")
         
         # Track entities (matches original: uses set not dict)
         self.entities = set()
